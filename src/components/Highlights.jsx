235b87d51e1b29eddabae7c9611b2e4a6f37cef1
Sergio <sergiof_2001@hotmail.com>
2022-12-11 23:46:25 -0800

    using react

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Highlights = () => {
+    return (
+        <section id='highlights'>
+            <div className='container'>
+                <div className='row'>
+                    <h2 className='section__title'>
+                        Why choose <span className='purple'>Library</span>
+                    </h2>
+                    <div className="highlight__Wrapper">
+                        <div className="highlight">
+                            <div className="highlight__img">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Highlights;
