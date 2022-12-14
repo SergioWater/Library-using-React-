355adb79e82da6dc83b4be6fdd2a524937fac3b3
Sergio <sergiof_2001@hotmail.com>
2022-12-13 23:21:40 -0800

    fixed landing page bug

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,16 +6,16 @@ const Nav = () => {
         <nav>
             <div className="nav__container">
                 <a href="/">
-                    <img src={LibraryLogo}  className='logo' alt="" />
+                    <img className="logo" src={LibraryLogo} alt="" />
                 </a>
                 <ul className='nav__links'>
                     <li className='nav__list'>
-                        <a href="" className='nav__link'>
+                        <a href="/" className='nav__link'>
                             Home
                         </a>
                     </li>
                     <li className='nav__list'>
-                        <a href="" className='nav__link'>
+                        <a href="/books" className='nav__link nav__link--primary'>
                             Books
                         </a>
                     </li>
--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@ library.add(faBars, faShoppingCart, faTimes, faBolt, faBookOpen, faTags)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
